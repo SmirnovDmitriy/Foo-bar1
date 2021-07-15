74b0a2d4b9d71bcb6b0a78a26e51624f99a32d0b
SmirnovDmitriy <dimonchik-92@mail.ru>
2021-07-16 01:49:58 +0300

    321

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+int i(1);
+void num(int);
+int main()
+{
+    int x;
+    std::cin >> x;
+    if (x <= 0)
+    {
+        std::cout << "Error!!!\n";
+        return 0;
+    }  
+    else
+    {
+        num(x); 
+    }
+}
+
+void num(int x)
+{
+    if (i <= x) 
+    {
+        if (i % 3 == 0)
+        {
+            std::cout << i << "Bar" << "\n";
+            i++;
+            num(x);
+        }
+        else
+        {
+            std::cout << i << "Foo" << "\n";
+            i++;
+            num(x);
+        }
+    }
+    else return;   
+}
